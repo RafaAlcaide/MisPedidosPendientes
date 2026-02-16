cb6e49ea4b38fbb221bd43ae5eceffecfde44ffb
Rafael Alcaide <153728890+RafaAlcaide@users.noreply.github.com>
2026-02-16 04:53:59 +0100

    Refactor AccesoRemoto for improved structure and error handling

--- app/src/main/java/es/studium/mispedidospendientes/AccesoRemoto.java
+++ app/src/main/java/es/studium/mispedidospendientes/AccesoRemoto.java
@@ -4,71 +4,77 @@ import okhttp3.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class AccesoRemoto
-{
-    private static final String URL_PEDIDOS = "http://10.0.2.2/APIRest/pedidos.php";
-    private static final String URL_TIENDAS = "http://10.0.2.2/APIRest/tiendas.php";
-    private OkHttpClient client = new OkHttpClient();
+public class AccesoRemoto {
 
-    public List<Pedido> obtenerPedidos() throws IOException, JSONException
-    {
-        Request request = new Request.Builder().url(URL_PEDIDOS).get().build();
-        Response response = client.newCall(request).execute();
+    private static final String URL_PEDIDOS = "http://10.0.2.2/ApiRest/pedidos.php";
+    private static final String URL_TIENDAS = "http://10.0.2.2/ApiRest/tiendas.php";
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    // -------------------- PEDIDOS --------------------
+    // -------------------- OBTENER PEDIDOS --------------------
+    public List<Pedido> obtenerPedidos() throws IOException, JSONException {
+
         List<Pedido> pedidos = new ArrayList<>();
 
-        if (response.body() != null)
-        {
-            String responseData = response.body().string();
-            JSONArray jsonArray = new JSONArray(responseData);
-            Map<Integer, String> tiendasMap = obtenerTiendasMap();
+        Request request = new Request.Builder().url(URL_PEDIDOS).get().build();
+        Response response = null;
 
-            for (int i = 0; i < jsonArray.length(); i++)
-            {
-                JSONObject jsonPedido = jsonArray.getJSONObject(i);
-                int idPedido = jsonPedido.getInt("idPedido");
-                String fechaPedido = jsonPedido.getString("fechaPedido");
-                String fechaEstimadaPedido = jsonPedido.getString("fechaEstimadaPedido");
-                String descripcionPedido = jsonPedido.getString("descripcionPedido");
-                double importePedido = jsonPedido.getDouble("importePedido");
-                int estadoPedido = jsonPedido.getInt("estadoPedido");
-                int idTienda = jsonPedido.getInt("idTiendaFK");
-                String nombreTienda = tiendasMap.getOrDefault(idTienda, "Desconocida");
+        Map<Integer, String> tiendasMap = obtenerTiendasMap();
 
-                if (estadoPedido == 0)
-                {
-                    pedidos.add(new Pedido(idPedido, fechaPedido, fechaEstimadaPedido, descripcionPedido, importePedido, estadoPedido, idTienda, nombreTienda));
+        try {
+            response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new IOException("Error HTTP: " + response.code());
+            }
+            if (response.body() != null) {
+                String responseData = response.body().string();
+                JSONArray jsonArray = new JSONArray(responseData);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonPedido = jsonArray.getJSONObject(i);
+
+                    // Obtener los datos del pedido
+                    int idPedido = jsonPedido.getInt("idPedido");
+                    String fechaPedido = jsonPedido.getString("fechaPedido");
+                    String fechaEstimadaPedido = jsonPedido.getString("fechaEstimadaPedido");
+                    String descripcionPedido = jsonPedido.getString("descripcionPedido");
+                    double importePedido = jsonPedido.getDouble("importePedido");
+                    int estadoPedido = jsonPedido.getInt("estadoPedido");
+                    int idTienda = jsonPedido.getInt("idTiendaFK");
+
+                    String nombreTienda = tiendasMap.getOrDefault(idTienda, "Desconocida");
+
+                    if (estadoPedido == 0) {
+                        pedidos.add(new Pedido(
+                                idPedido,
+                                fechaPedido,
+                                fechaEstimadaPedido,
+                                descripcionPedido,
+                                importePedido,
+                                estadoPedido,
+                                idTienda,
+                                nombreTienda
+                        ));
+                    }
                 }
+            }
+        } finally {
+            if (response != null && response.body() != null) {
+                response.body().close();
             }
         }
         return pedidos;
     }
 
-    public Map<Integer, String> obtenerTiendasMap() throws IOException, JSONException
-    {
-        Map<Integer, String> tiendas = new HashMap<>();
-        Request request = new Request.Builder().url(URL_TIENDAS).get().build();
-        Response response = client.newCall(request).execute();
-        if (response.body() != null) {
-            String responseData = response.body().string();
-            JSONArray jsonArray = new JSONArray(responseData);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonTienda = jsonArray.getJSONObject(i);
-                int idTienda = jsonTienda.getInt("idTienda");
-                String nombreTienda = jsonTienda.getString("nombreTienda");
-                tiendas.put(idTienda, nombreTienda);
-            }
-        }
-        return tiendas;
-    }
+    // -------------------- AGREGAR PEDIDOS --------------------
+    public void agregarPedido(int idTienda, String fechaEntrega,
+                              String descripcion, String importe) throws IOException {
 
-    public void agregarPedido(int idTienda, String fechaEntrega, String descripcion, String importe) throws IOException
-    {
         RequestBody body = new FormBody.Builder()
                 .add("fechaEstimadaPedido", fechaEntrega)
                 .add("descripcionPedido", descripcion)
@@ -81,63 +87,127 @@ public class AccesoRemoto
                 .post(body)
                 .build();
 
-        Response response = client.newCall(request).execute();
-        if (response.isSuccessful()) {
-            System.out.println("Pedido agregado correctamente");
-        } else {
-            System.out.println("Error al agregar el pedido" + response.code());
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new IOException("Error al agregar pedido. Código: " + response.code());
+            }
+        } finally {
+            if (response != null && response.body() != null) {
+                response.body().close();
+            }
         }
     }
 
-    public void actualizarPedido(int idPedido, String fechaPedido, String fechaEntrega, String descripcion, String importe, int estado, int idTienda) throws IOException {
-        String url = URL_PEDIDOS + "?idPedido=" + idPedido +
-                "&fechaPedido=" + fechaPedido +
-                "&fechaEstimadaPedido=" + fechaEntrega +
-                "&descripcionPedido=" + descripcion +
-                "&importePedido=" + importe +
-                "&estadoPedido=" + estado +
-                "&idTiendaFK=" + idTienda;
+    // -------------------- ACTUALIZAR PEDIDOS --------------------
+    public void actualizarPedido(int idPedido, String fechaPedido, String fechaEstimada, String desc,
+                                 String imp, int estado, int idTienda) throws IOException {
 
-        Request request = new Request.Builder()
-                .url(url)
-                .put(RequestBody.create(null, new byte[0]))
-                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+        HttpUrl urlFinal = HttpUrl.parse(URL_PEDIDOS).newBuilder()
+                .addQueryParameter("idPedido", String.valueOf(idPedido))
+                .addQueryParameter("fechaPedido", fechaPedido)
+                .addQueryParameter("fechaEstimadaPedido", fechaEstimada)
+                .addQueryParameter("descripcionPedido", desc)
+                .addQueryParameter("importePedido", imp)
+                .addQueryParameter("estadoPedido", String.valueOf(estado))
+                .addQueryParameter("idTiendaFK", String.valueOf(idTienda))
                 .build();
 
-        client.newCall(request).execute();
+        RequestBody cuerpoVacio = RequestBody.create(MediaType.parse("text/plain"), "");
+
+        Request request = new Request.Builder()
+                .url(urlFinal)
+                .put(cuerpoVacio)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("El servidor no respondió correctamente");
+        }
+        if (response.body() != null) response.body().close();
     }
 
-    public void eliminarPedido(int idPedido) throws IOException
-    {
-        String url = URL_PEDIDOS + "?idPedido=" + idPedido;
+    // -------------------- ELIMINAR PEDIDOS --------------------
+    public void eliminarPedido(int idPedido) throws IOException {
+        HttpUrl url = HttpUrl.parse(URL_PEDIDOS).newBuilder()
+                .addQueryParameter("idPedido", String.valueOf(idPedido))
+                .build();
+
         Request request = new Request.Builder()
                 .url(url)
                 .delete()
                 .build();
 
-        client.newCall(request).execute();
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Error al eliminar pedido. Código: " + response.code());
+        }
+        if (response.body() != null) response.body().close();
     }
 
-    public List<Tienda> obtenerTiendas() throws IOException, JSONException
-    {
+    // -------------------- TIENDAS --------------------
+
+    // -------------------- OBTENER TIENDAS (MAP) --------------------
+    public Map<Integer, String> obtenerTiendasMap() throws IOException, JSONException {
+        Map<Integer, String> tiendas = new HashMap<>();
         Request request = new Request.Builder().url(URL_TIENDAS).get().build();
-        Response response = client.newCall(request).execute();
-        List<Tienda> tiendas = new ArrayList<>();
-        if (response.body() != null) {
-            String responseData = response.body().string();
-            JSONArray jsonArray = new JSONArray(responseData);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonTienda = jsonArray.getJSONObject(i);
-                int idTienda = jsonTienda.getInt("idTienda");
-                String nombreTienda = jsonTienda.getString("nombreTienda");
-                tiendas.add(new Tienda(idTienda, nombreTienda));
+        Response response = null;
+
+        try {
+            response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new IOException("Error HTTP: " + response.code());
+            }
+            if (response.body() != null) {
+                String responseData = response.body().string();
+                JSONArray jsonArray = new JSONArray(responseData);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonTienda = jsonArray.getJSONObject(i);
+                    int idTienda = jsonTienda.getInt("idTienda");
+                    String nombreTienda = jsonTienda.getString("nombreTienda");
+                    tiendas.put(idTienda, nombreTienda);
+                }
+            }
+        } finally {
+            if (response != null && response.body() != null) {
+                response.body().close();
             }
         }
         return tiendas;
     }
 
-    public void agregarTienda(String nombreTienda) throws IOException
-    {
+    // -------------------- OBTENER TIENDAS (LIST) --------------------
+    public List<Tienda> obtenerTiendas() throws IOException, JSONException {
+        List<Tienda> listaTiendas = new ArrayList<>();
+        Request request = new Request.Builder().url(URL_TIENDAS).get().build();
+        Response response = null;
+
+        try {
+            response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new IOException("Error HTTP: " + response.code());
+            }
+            if (response.body() != null) {
+                String responseData = response.body().string();
+                JSONArray jsonArray = new JSONArray(responseData);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonTienda = jsonArray.getJSONObject(i);
+                    int idTienda = jsonTienda.getInt("idTienda");
+                    String nombreTienda = jsonTienda.getString("nombreTienda");
+                    listaTiendas.add(new Tienda(idTienda, nombreTienda));
+                }
+            }
+        } finally {
+            if (response != null && response.body() != null) {
+                response.body().close();
+            }
+        }
+        return listaTiendas;
+    }
+
+    // -------------------- AGREGAR TIENDAS --------------------
+    public void agregarTienda(String nombreTienda) throws IOException {
         RequestBody body = new FormBody.Builder()
                 .add("nombreTienda", nombreTienda)
                 .build();
@@ -147,28 +217,52 @@ public class AccesoRemoto
                 .post(body)
                 .build();
 
-        client.newCall(request).execute();
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Error al agregar tienda. Código: " + response.code());
+        }
+        if (response.body() != null) response.body().close();
     }
 
-    public void actualizarTienda(int idTienda, String nuevoNombre) throws IOException
-    {
-        String url = URL_TIENDAS + "?idTienda=" + idTienda + "&nombreTienda=" + nuevoNombre;
-        Request request = new Request.Builder()
-                .url(url)
-                .put(RequestBody.create(null, new byte[0]))
+    // -------------------- ACTUALIZAR TIENDAS (AHORA IGUAL QUE PEDIDOS) --------------------
+    public void actualizarTienda(int idTienda, String nuevoNombre) throws IOException {
+        // Usamos addQueryParameter para idTienda y nombreTienda tal cual haces en Pedidos
+        HttpUrl url = HttpUrl.parse(URL_TIENDAS).newBuilder()
+                .addQueryParameter("idTienda", String.valueOf(idTienda))
+                .addQueryParameter("nombreTienda", nuevoNombre)
                 .build();
 
-        client.newCall(request).execute();
+        // Creamos el cuerpo vacío con el mismo MediaType que Pedidos
+        RequestBody cuerpoVacio = RequestBody.create(MediaType.parse("text/plain"), "");
+
+        // Usamos PUT, que es el método que te funciona en actualizarPedido
+        Request request = new Request.Builder()
+                .url(url)
+                .put(cuerpoVacio)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Error al actualizar tienda. Código: " + response.code());
+        }
+        if (response.body() != null) response.body().close();
     }
 
-    public void eliminarTienda(int idTienda) throws IOException
-    {
-        String url = URL_TIENDAS + "?idTienda=" + idTienda;
+    // -------------------- ELIMINAR TIENDAS (AHORA IGUAL QUE PEDIDOS) --------------------
+    public void eliminarTienda(int idTienda) throws IOException {
+        HttpUrl url = HttpUrl.parse(URL_TIENDAS).newBuilder()
+                .addQueryParameter("idTienda", String.valueOf(idTienda))
+                .build();
+
         Request request = new Request.Builder()
                 .url(url)
                 .delete()
                 .build();
 
-        client.newCall(request).execute();
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Error al eliminar tienda. Código: " + response.code());
+        }
+        if (response.body() != null) response.body().close();
     }
 }
